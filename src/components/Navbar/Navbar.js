41375f335b206e53dceba58672ff0cd817817111
LezhnveSergei <catisfat1337@gmail.com>
2020-03-30 16:13:58 +0500

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+
+const NavigationLinks = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route/>
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default NavigationLinks;
